ae15e6fb5b60068bc896d619914b848408f1d697
SubainaNorab <subainanorab@gmail.com>
2025-05-15 15:55:15 +0500

    Updated dataset

--- dataset.py
+++ dataset.py
@@ -2,15 +2,15 @@ import csv
 import random
 from datetime import datetime, timedelta
 
-num_records = 300
-V_CLEAN = 0.9
+num_records = 300 # record
+V_CLEAN = 0.9 # to compare dust level
 K = 0.5
 start_time = datetime.now()
 
 with open("solar_clean_data.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["date", "time", "dust_value", "voltage", "current", "label"])
-
+# generating data
     for i in range(num_records):
         now = start_time + timedelta(minutes=10 * i)
         date = now.strftime("%Y-%m-%d")
@@ -19,10 +19,10 @@ with open("solar_clean_data.csv", "w", newline="") as file:
         vo = round(random.uniform(0.8, 3.6), 2)
         dust_value = max((vo - V_CLEAN) / K, 0)
 
-        voltage = round(random.uniform(16, 20) - dust_value * 0.1, 2)
-        current = round(random.uniform(2000, 3000) * (1 - dust_value / 15), 2)
+        voltage = round(random.uniform(16, 20) - dust_value * 0.1, 2) #panel voltage
+        current = round(random.uniform(2000, 3000) * (1 - dust_value / 15), 2) # panel current
 
-        # Human-readable label
+        # label
         label = "needs_cleaning" if dust_value > 3 and current < 2000 else "clean"
 
         writer.writerow([date, time, dust_value, voltage, current, label])
